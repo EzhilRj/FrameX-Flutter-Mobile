bd4b01a424a2eb2702114e2525d883438e0b63a5
EzhilRj <ezhilrj24@gmail.com>
2023-07-12 18:02:31 +0530

    Start full application Automation.

--- src/main/java/UiObjects/CallPlan_Objects.java
+++ src/main/java/UiObjects/CallPlan_Objects.java
@@ -7,9 +7,10 @@ import static Utilities.DBConfig.GetDataObject;
 public class CallPlan_Objects {
 
 	public static String sync = "Sync";
-	public static String tid = "10539";
+	public static String tid = "13018";
 	public static String TargetID = "//android.view.View[contains(@content-desc, 'Target ID: "+tid+"')]";
-	public static String notification = "//*[@text='Target "+tid+" downloaded successfully.']";
+	public static String targetdownloadnotification = "//*[@text='Target "+tid+" downloaded successfully.']";
+	public static String targetUploadnotification =  "//*[@text='Uploading process for "+tid+" is Completed']";  ////android.view.View[@content-desc="You have already uploaded 14906 target"]
 	public static String clear = "//android.widget.TextView[@content-desc='Clear,Button']";
 	public static String TodayCalls = "//android.view.View[contains(@content-desc, 'Today Calls')]";
 	public static String Startworkbutton = "//android.view.View[@content-desc='Start Work']";
@@ -32,7 +33,6 @@ public class CallPlan_Objects {
 
 		return FormslistsLocator;
 	}
-
 	public static String DataTypeGetter(String form) throws Exception {
 
 		List<Object> fieldnames = GetDataObject("select FieldName,DataType,ControlType from FormFieldsDetail where [Form Name] = '"+form+"'");
@@ -50,54 +50,26 @@ public class CallPlan_Objects {
 	public static String CloseCallButton = "//android.view.View[@content-desc='Close Call']";
 
 	public static String Perfectstorescorepopup = "//android.view.View[@content-desc='Perfect Store Score']";
-
 	//Acc id
 	public static String Okbutton = "ok";
-
 	//Acc id
 	//pss succss msg
 	public static String psssuccs = "Pss Upload Success";
-
 	//Acc id
 	public static String Yesbutton = "Yes";
-
 	//Acc id
 	public static String Nobutton = "No";
-
 	//Acc id
 	public static String Uploadcallconfirmpopup = "Are you sure you want to upload Call ?";
-
 	public static String Attendancepopup = "Please also mark your Attendance for today!";
-
 	public static String Attendancdropdown = "//android.widget.Button[@content-desc='Present']";
 	public static String Submit = "Submit";
-
 	public static String Attendancecamera = "//android.widget.ImageView[1]";
-
 	public static String Frontcamerabutton = "//android.widget.ImageView[4]";
-
 	public static String Attendacealreadysaved = "Attendance data is already saved";
-
 	public static String Underprocesspopup = "Attendance is under process  please Proceed to next step ?";
 	public static String Viewimagesbutton = "View Images";
-
 	public static String Uploadcallsbutton = "Upload Calls";
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
